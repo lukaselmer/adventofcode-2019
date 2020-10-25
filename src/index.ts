7f6cc81ed7a3b3f1177f8aa6ae008dbbde664c62
Lukas Elmer <lukas.elmer@gmail.com>
2020-10-25 18:08:14 +0100

    Day 1 star 2

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { input1 } from './inputs'
+import { calcSingleFuelRequirements } from './part1star1'
 
 function main() {
-  console.log(
-    input1
-      .split('\n')
-      .map(calcSingleFuelRequirements)
-      .reduce((sum, current) => sum + current, 0)
-  )
-}
-
-function calcSingleFuelRequirements(el: string): number {
-  const number = parseInt(el, 10)
-  return Math.floor(number / 3) - 2
+  const massesToProcess = input1.split('\n').map((el) => parseInt(el, 10))
+  let currentFuel = 0
+  while (massesToProcess.length > 0) {
+    const currentMass = massesToProcess.shift()
+    if (!currentMass) continue
+    const fuelForCurrentMass = calcSingleFuelRequirements(currentMass)
+    if (fuelForCurrentMass > 0) {
+      currentFuel += fuelForCurrentMass
+      massesToProcess.push(fuelForCurrentMass)
+    }
+  }
+  console.log(currentFuel)
 }
 
 main()
