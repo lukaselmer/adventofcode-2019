c02ecfd77723e1c82fc66e90bb8b2eee5fca6a04
Lukas Elmer <lukas.elmer@gmail.com>
2020-10-26 21:00:46 +0100

    Day 3 star 2

--- src/a3s1.ts
+++ src/a3s1.ts
@@ -4,18 +4,31 @@ function main(wire1: string, wire2: string) {
   const board = new Board()
   board.addWire(wire1.split(','), 1)
   board.addWire(wire2.split(','), 2)
+  board.addWire(wire1.split(','), 1)
   const centralPort = board.centralPort
   const distances = board.crossings.map((crossing) => crossing.distance(centralPort))
-  return Math.min(...distances)
+
+  return {
+    minCentralPortDistance: Math.min(...distances),
+    firstIntersectionDistance: board.firstIntersectionDistance,
+  }
 }
+
+type WireID = number
+type Distance = number
 
 class Board {
   private maxPoint = pointFactory.newPoint(0, 0)
   private minPoint = pointFactory.newPoint(0, 0)
-  private content: Map<Point, Set<number>> = new Map()
+  private content: Map<Point, Set<WireID>> = new Map()
   private _crossings: Set<Point> = new Set()
+  private crossingDistances: Map<Point, Record<WireID, Distance>> = new Map()
+  private wires: Set<WireID> = new Set()
 
-  addWire(wire: string[], wireNumber: number) {
+  addWire(wire: string[], wireNumber: WireID) {
+    this.wires.add(wireNumber)
+
+    let distanceFromStart = 0
     let currentPoint = this.centralPort
     while (wire.length > 0) {
       const instruction = this.parseInstruction(wire.shift())
@@ -23,9 +36,10 @@ class Board {
 
       let [direction, distance] = instruction
       while (distance > 0) {
-        --distance
         currentPoint = this.movePoint(direction, currentPoint)
-        this.mark(currentPoint, wireNumber)
+        --distance
+        ++distanceFromStart
+        this.mark(currentPoint, wireNumber, distanceFromStart)
       }
     }
   }
@@ -51,19 +65,24 @@ class Board {
     return [direction as Direction, distance] as const
   }
 
-  mark(point: Point, wire: number) {
+  private mark(point: Point, wire: WireID, distanceFromStart: Distance) {
     this.minPoint = this.minPoint.min(point)
     this.maxPoint = this.maxPoint.max(point)
     const set = this.setFor(point)
     set.add(wire)
-    if (set.size > 1) this._crossings.add(point)
+    if (set.size > 1) {
+      this._crossings.add(point)
+      const crossingDistance = this.crossingDistances.get(point) || {}
+      crossingDistance[wire] = Math.min(crossingDistance[wire] || Infinity, distanceFromStart)
+      this.crossingDistances.set(point, crossingDistance)
+    }
   }
 
   private setFor(point: Point) {
     const set = this.content.get(point)
     if (set) return set
 
-    const newSet = new Set<number>()
+    const newSet = new Set<WireID>()
     this.content.set(point, newSet)
     return newSet
   }
@@ -78,6 +97,18 @@ class Board {
 
   get crossings() {
     return [...this._crossings.keys()]
+  }
+
+  get firstIntersectionDistance() {
+    const wires = [...this.wires.keys()]
+    if (wires.length !== 2) throw new Error(`Meh ${this.wires}`)
+    const [wire1, wire2] = wires
+    const distances = this.crossings.map((crossing) => {
+      const crossingDistances = this.crossingDistances.get(crossing)!
+      return crossingDistances[wire1] + crossingDistances[wire2]
+    })
+
+    return Math.min(...distances)
   }
 }
 
@@ -150,16 +181,22 @@ export function assertNever(value: never): never {
 }
 
 function assertEquals<T>(a: T, b: T) {
-  if (a !== b) console.log(`Expected ${a} == ${b}`)
+  if (JSON.stringify(a) !== JSON.stringify(b)) console.log(`Expected ${a} == ${b}`)
 }
 
 const pointFactory = new PointFactory()
 
-assertEquals(main('R8,U5,L5,D3', 'U7,R6,D4,L4'), 6)
-assertEquals(main('R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'), 159)
+assertEquals(main('R8,U5,L5,D3', 'U7,R6,D4,L4'), {
+  minCentralPortDistance: 6,
+  firstIntersectionDistance: 30,
+})
+assertEquals(main('R75,D30,R83,U83,L12,D49,R71,U7,L72', 'U62,R66,U55,R34,D71,R55,D58,R83'), {
+  minCentralPortDistance: 159,
+  firstIntersectionDistance: 610,
+})
 assertEquals(
   main('R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51', 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'),
-  135
+  { minCentralPortDistance: 135, firstIntersectionDistance: 410 }
 )
 
-console.log('S1: ', main(...input3))
+console.log(main(...input3))
